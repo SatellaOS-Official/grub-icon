88392f9da0f16f6d13977c5c98eeafe26e287c61
satella <kiyotakaa591@gmail.com>
2025-12-04 15:40:02 +0300

    Update grub-icon.sh

--- grub-icon.sh
+++ grub-icon.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 shopt -s nullglob
 
-# SatellaOS GRUB simgesi dizinini oluştur
 sudo mkdir -p /usr/share/SatellaOS/grub-icon/
 
-# GRUB simgesi dosyalarını kopyala
 sudo cp ~/git-script/grub-icon/*.{jpg,png} /usr/share/SatellaOS/grub-icon/
 
-# Dosya izinlerini ayarla
 sudo chmod 655 /usr/share/SatellaOS/grub-icon/*
 
-# Kullanıcı için özel satella-picture klasörü oluştur
 mkdir -p ~/satella-picture
 
-# Bu klasöre sembolik link ekle
 ln -s /usr/share/SatellaOS/grub-icon ~/satella-picture/grub-icon 2>/dev/null
-
-echo "✔ GRUB simgesi başarıyla yüklendi!"
